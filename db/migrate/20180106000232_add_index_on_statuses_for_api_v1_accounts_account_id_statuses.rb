c4ded6a7b73cbb50ad983c307981ac8861e78bae
ABCanG <abcang1015@gmail.com>
2018-01-15 14:44:22 +0900

    Fix migration file

--- db/migrate/20180106000232_add_index_on_statuses_for_api_v1_accounts_account_id_statuses.rb
+++ db/migrate/20180106000232_add_index_on_statuses_for_api_v1_accounts_account_id_statuses.rb
@@ -2,9 +2,9 @@ class AddIndexOnStatusesForApiV1AccountsAccountIdStatuses < ActiveRecord::Migrat
   disable_ddl_transaction!
 
   def change
-    # safety_assured do
+    safety_assured do
       add_index :statuses, [:account_id, :id, :visibility, :updated_at], order: { id: :desc }, algorithm: :concurrently, name: :index_statuses_20180106
-    # end
+    end
     remove_index :statuses, name: :index_statuses_on_account_id_id
   end
 end
