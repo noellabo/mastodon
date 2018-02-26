8e6d323f4d538040d4693efd6e8a75e4dbf26bad
ABCanG <abcang1015@gmail.com>
2018-02-26 10:00:39 +0900

    Revert "Merge pull request #865 from pixiv/revert-847-onboarding"
    This reverts commit e1d42812a7f0fbbe09a76047697c56f3809ec7c4, reversing
    changes made to fb21f4d4e8400162e09cd74943e794b24ec5ac01.

--- app/javascript/mastodon/features/ui/containers/columns_area_container.js
+++ app/javascript/mastodon/features/ui/containers/columns_area_container.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import ColumnsArea from '../components/columns_area';
+import { getColumns } from '../../../../pawoo/util/selectors';
 
 const mapStateToProps = state => ({
-  columns: state.getIn(['settings', 'columns']),
+  columns: getColumns(state),
   isModalOpen: !!state.get('modal').modalType,
+  pawooPage: state.getIn(['pawoo', 'page']),
 });
 
 export default connect(mapStateToProps, null, null, { withRef: true })(ColumnsArea);
---
+++ app/javascript/pawoo/actions/page.js
@@ -0,0 +1,5 @@
+export const PAGE_SET = 'PAWOO_PAGE_SET';
+
+export function setPage(page) {
+  return { type: PAGE_SET, page };
+}
