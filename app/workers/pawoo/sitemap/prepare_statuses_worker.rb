de65103998e97789550e948f482cf63f7f3beb9b
ABCanG <abcang1015@gmail.com>
2018-03-28 16:21:20 +0900

    Fix preparing sitemap at the same time

--- app/workers/pawoo/sitemap/prepare_statuses_worker.rb
+++ app/workers/pawoo/sitemap/prepare_statuses_worker.rb
@@ -6,16 +6,47 @@ class Pawoo::Sitemap::PrepareStatusesWorker
 
   sidekiq_options queue: 'pull', unique: :until_executed
 
-  def perform(page, load_next_page = false)
-    return if load_next_page
+  def perform(page, continuously_key = nil)
+    if continuously_key
+      perform_continuously(page, continuously_key)
+    else
+      prepare_sitemap(page)
+    end
+  end
 
+  private
+
+  def perform_continuously(page, continuously_key)
+    return if page == 1 && !redis.setnx(redis_lock_key, continuously_key)
+    return if page > 1 && redis.get(redis_lock_key) != continuously_key
+
+    prepare_sitemap(page)
+
+    next_page = page + 1
+    if next_page <= page_count
+      Pawoo::Sitemap::PrepareStatusesWorker.perform_async(next_page, continuously_key)
+    else
+      redis.del(redis_lock_key)
+    end
+  end
+
+  def redis
+    Redis.current
+  end
+
+  def redis_lock_key
+    "pawoo:sitemap:prepare_statuses"
+  end
+
+  def prepare_sitemap(page)
     read_from_slave do
       Pawoo::Sitemap::Status.new(page).prepare
+    end
+  end
 
-      if load_next_page
-        next_page = page + 1
-        Pawoo::Sitemap::PrepareStatusesWorker.perform_async(next_page, true) if next_page <= Pawoo::Sitemap::Status.page_count
-      end
+  def page_count
+    read_from_slave do
+      Pawoo::Sitemap::Status.page_count
     end
   end
 end
--- spec/workers/pawoo/prepare_statuses_worker_spec.rb
+++ spec/workers/pawoo/prepare_statuses_worker_spec.rb
@@ -9,11 +9,45 @@ describe Pawoo::Sitemap::PrepareStatusesWorker do
     stub_const 'Pawoo::Sitemap::SITEMAPINDEX_SIZE', 1
   end
 
-  describe 'perform' do
-    let(:status) { Fabricate(:status, reblogs_count: 5) }
+  around do |example|
+    Sidekiq::Testing.fake! do
+      example.run
+    end
+  end
 
-    context 'when load_next_page is false' do
+  describe 'perform' do
+    let!(:status) { Fabricate(:status, reblogs_count: 5) }
+    let(:sitemap) { double }
+    let(:continuously_key) { nil }
+
+    shared_examples 'prepares sitemap' do
+      it 'prepares sitemap' do
+        allow(Pawoo::Sitemap::Status).to receive(:new).and_return(sitemap)
+        allow(sitemap).to receive(:prepare)
+
+        subject.perform(page, continuously_key)
+
+        expect(Pawoo::Sitemap::Status).to have_received(:new).with(page)
+        expect(sitemap).to have_received(:prepare)
+      end
+    end
+
+    shared_examples 'does nothing' do
+      it 'does nothing' do
+        allow(Pawoo::Sitemap::Status).to receive(:new).and_return(sitemap)
+        allow(sitemap).to receive(:prepare)
+
+        subject.perform(page, continuously_key)
+
+        expect(Pawoo::Sitemap::Status).not_to have_received(:new).with(page)
+        expect(sitemap).not_to have_received(:prepare)
+      end
+    end
+
+    context 'when load_next_page is not set' do
       let(:page) { status.stream_entry.id }
+
+      include_examples 'prepares sitemap'
 
       it 'does not run worker' do
         allow(Pawoo::Sitemap::PrepareStatusesWorker).to receive(:perform_async)
@@ -23,15 +57,54 @@ describe Pawoo::Sitemap::PrepareStatusesWorker do
       end
     end
 
-    xcontext 'when load_next_page is true' do
+    context 'when load_next_page is set' do
+      let(:continuously_key) { 'continuously_key' }
+
+      context 'when page is the first' do
+        let(:page) { 1 }
+
+        context 'when there is no lock key' do
+          include_examples 'prepares sitemap'
+
+          it 'set lock key' do
+            subject.perform(page, continuously_key)
+            expect(Redis.current.get("pawoo:sitemap:prepare_statuses")).to eq continuously_key
+          end
+        end
+
+        context 'when there is a lock key' do
+          before do
+            Redis.current.set("pawoo:sitemap:prepare_statuses", 'continuously_key')
+          end
+
+          include_examples 'does nothing'
+        end
+      end
+
       context 'when page is not the last' do
         let(:page) { status.stream_entry.id }
 
-        it 'runs worker with next page' do
-          allow(Pawoo::Sitemap::PrepareStatusesWorker).to receive(:perform_async)
+        context 'when the lock keys match' do
+          before do
+            Redis.current.set("pawoo:sitemap:prepare_statuses", continuously_key)
+          end
 
-          subject.perform(page, true)
-          expect(Pawoo::Sitemap::PrepareStatusesWorker).to have_received(:perform_async).with(page + 1, true)
+          include_examples 'prepares sitemap'
+
+          it 'runs worker with next page' do
+            allow(Pawoo::Sitemap::PrepareStatusesWorker).to receive(:perform_async)
+
+            subject.perform(page, continuously_key)
+            expect(Pawoo::Sitemap::PrepareStatusesWorker).to have_received(:perform_async).with(page + 1, continuously_key)
+          end
+        end
+
+        context 'when the lock keys do not match' do
+          before do
+            Redis.current.set("pawoo:sitemap:prepare_statuses", 'another_continuously_key')
+          end
+
+          include_examples 'does nothing'
         end
       end
 
@@ -41,8 +114,14 @@ describe Pawoo::Sitemap::PrepareStatusesWorker do
         it 'does not run worker' do
           allow(Pawoo::Sitemap::PrepareStatusesWorker).to receive(:perform_async)
 
-          subject.perform(page, true)
+          subject.perform(page, continuously_key)
           expect(Pawoo::Sitemap::PrepareStatusesWorker).not_to have_received(:perform_async)
+        end
+
+        it 'deletes lock key' do
+          subject.perform(page, continuously_key)
+
+          expect(Redis.current.exists("pawoo:sitemap:prepare_statuses")).to be false
         end
       end
     end
