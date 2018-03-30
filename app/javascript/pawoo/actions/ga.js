ade54664db6eef7dc44026826a5e591b7f3145ce
abcang <abcang1015@gmail.com>
2018-03-30 17:12:42 +0900

    Fix condition of GA

--- app/javascript/pawoo/actions/ga.js
+++ app/javascript/pawoo/actions/ga.js
@@ -2,13 +2,14 @@
 
 const isProduction = process.env.NODE_ENV === 'production';
 const debug = false; // NOTE: trueにすることでgaのコマンドの内容がconsoleに表示される
+const enableDebug = (!isProduction && debug);
 
 // Load GA
 /* eslint-disable */
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script',`https://www.google-analytics.com/analytics${debug ? '_debug' : ''}.js`,'ga');
+})(window,document,'script',`https://www.google-analytics.com/analytics${enableDebug ? '_debug' : ''}.js`,'ga');
 /* eslint-enable */
 
 const option = (() => {
@@ -23,12 +24,12 @@ const option = (() => {
 })();
 
 ga('create', 'UA-1830249-136', option);
-if (debug) {
+if (enableDebug) {
   ga('set', 'sendHitTask', null);
 }
 
 function disableGa(path = window.location.pathname) {
-  return !debug || isProduction || path.indexOf('/admin/') === 0;
+  return !isProduction || !debug || path.indexOf('/admin/') === 0;
 }
 
 export const event = (params) => {
