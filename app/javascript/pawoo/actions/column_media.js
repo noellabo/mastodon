953993cebd9b0d096e045a14f8c0638531fcc649
abcang <abcang1015@gmail.com>
2018-04-16 14:16:29 +0900

    Delete first anniversary

--- app/javascript/pawoo/actions/column_media.js
+++ app/javascript/pawoo/actions/column_media.js
@@ -3,8 +3,8 @@ export const COLUMN_MEDIA_RESIZE = 'PAWOO_COLUMN_MEDIA_RESIZE';
 export function resizeColumnMedia(single) {
   return (dispatch, getState) => dispatch({
     type: COLUMN_MEDIA_RESIZE,
-    columnCount: getState().getIn(['pawoo', 'page']) === 'PAWOO_FIRST_ANNIVERSARY' ? 1 : getState().getIn(['settings', 'columns']).count(),
-    defaultPage: ['DEFAULT', 'PAWOO_FIRST_ANNIVERSARY'].includes(getState().getIn(['pawoo', 'page'])),
+    columnCount: getState().getIn(['settings', 'columns']).count(),
+    defaultPage: getState().getIn(['pawoo', 'page']) === 'DEFAULT',
     single,
     window: { innerWidth, innerHeight },
   });
