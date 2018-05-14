27d385f3b952a6512114886d421e648721ff544f
abcang <abcang1015@gmail.com>
2018-05-14 12:54:51 +0900

    rollback index_statuses_20180106

--- db/migrate/20180514122931_remove_index_statuses_20180106_on_statuses.rb
+++ db/migrate/20180514122931_remove_index_statuses_20180106_on_statuses.rb
@@ -1,5 +1,13 @@
 class RemoveIndexStatuses20180106OnStatuses < ActiveRecord::Migration[5.1]
-  def change
+  disable_ddl_transaction!
+
+  def up
     remove_index :statuses, name: :index_statuses_20180106
+  end
+
+  def down
+    safety_assured do
+      add_index :statuses, [:account_id, :id, :visibility, :updated_at], order: { id: :desc }, algorithm: :concurrently, name: :index_statuses_20180106
+    end
   end
 end
