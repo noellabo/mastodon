16f52ca07d7a875d0ab6ee799c4f6ff0dd743b58
abcang <abcang1015@gmail.com>
2018-05-24 17:37:46 +0900

    skip migration to add index

--- db/migrate/20180514140000_revert_index_change_on_statuses_for_api_v1_accounts_account_id_statuses.rb
+++ db/migrate/20180514140000_revert_index_change_on_statuses_for_api_v1_accounts_account_id_statuses.rb
@@ -4,11 +4,11 @@ class RevertIndexChangeOnStatusesForApiV1AccountsAccountIdStatuses < ActiveRecor
   disable_ddl_transaction!
 
   def change
-    safety_assured do
-      add_index :statuses, [:account_id, :id, :visibility, :updated_at], order: { id: :desc }, algorithm: :concurrently, name: :index_statuses_20180106
-    end
-
-    remove_index :statuses, column: [:account_id, :id, :visibility], where: 'visibility IN (0, 1, 2)', algorithm: :concurrently
-    remove_index :statuses, column: [:account_id, :id], where: 'visibility = 3', algorithm: :concurrently
+    # safety_assured do
+    #   add_index :statuses, [:account_id, :id, :visibility, :updated_at], order: { id: :desc }, algorithm: :concurrently, name: :index_statuses_20180106
+    # end
+    #
+    # remove_index :statuses, column: [:account_id, :id, :visibility], where: 'visibility IN (0, 1, 2)', algorithm: :concurrently
+    # remove_index :statuses, column: [:account_id, :id], where: 'visibility = 3', algorithm: :concurrently
   end
 end
