17c3f6e9e66bc01bd6732ba3e0716ee7388c774d
fugi2 <fugi@pixiv.com>
2018-08-20 20:43:05 +0900

    suggested account にtwitter連携ボタン追加

--- app/javascript/pawoo/components/suggested_accounts_column.js
+++ app/javascript/pawoo/components/suggested_accounts_column.js
@@ -12,6 +12,7 @@ import SuggestedAccountsContainer from '../containers/suggested_accounts_contain
 
 const messages = defineMessages({
   title: { id: 'column.suggested_accounts', defaultMessage: 'Active Users' },
+  mastodonBridge: { id: 'suggested_accounts.mastodon_bridge', defaultMessage: 'Search your Twitter friends' },
   goToLocalTimeline: { id: 'suggested_accounts.go_to_local_timeline', defaultMessage: 'Go To Local Timeline' },
 });
 
@@ -51,7 +52,15 @@ export default class SuggestedAccountsColumn extends React.PureComponent {
   render () {
     return this.props.multiColumn ? (
       <div className='pawoo-suggested-accounts-column--page'>
-        <div className='column'><ColumnBackButton /></div>
+        <div className='column'>
+          <ColumnBackButton />
+          <div class="mastodon_bridge">
+            <a class="column-link" href="https://bridge.joinmastodon.org/" target="_blank">
+              <i class="fa fa-fw fa-twitter column-link__icon"></i>
+              {this.props.intl.formatMessage(messages.mastodonBridge)}
+            </a>
+          </div>
+        </div>
         <img alt='' src={PawooUI} />
       </div>
     ) : (
