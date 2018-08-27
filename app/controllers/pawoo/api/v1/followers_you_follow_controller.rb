a54bc7ae3f3bebc8accc10477c0377632c06871b
wintu <wintu@abcangnoiMac.local>
2018-08-27 15:07:05 +0900

    limit修正

--- app/controllers/pawoo/api/v1/followers_you_follow_controller.rb
+++ app/controllers/pawoo/api/v1/followers_you_follow_controller.rb
@@ -10,7 +10,7 @@ class Pawoo::Api::V1::FollowersYouFollowController < Api::BaseController
     current_account_id = current_account.id
     return render json: [] if target_account.id == current_account_id
     target_followers = Follow.where(target_account_id: target_account.id).select(:account_id)
-    @followers_you_follow = Follow.where(account_id: current_account_id, target_account_id: target_followers).preload(target_account: :oauth_authentications).limit(6)
+    @followers_you_follow = Follow.where(account_id: current_account_id, target_account_id: target_followers).preload(target_account: :oauth_authentications).limit(30)
     render json: @followers_you_follow.map(&:target_account), each_serializer: REST::AccountSerializer
   end
 end
