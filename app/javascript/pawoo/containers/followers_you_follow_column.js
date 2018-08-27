18b396a19aaaa9f8736f2a9b8231871d8f6c3b3d
wintu <wintu@abcangnoiMac.local>
2018-08-27 14:56:52 +0900

    デザイン調整とGA導入

--- app/javascript/pawoo/containers/followers_you_follow_column.js
+++ app/javascript/pawoo/containers/followers_you_follow_column.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 import Avatar from '../../mastodon/components/avatar';
-import DisplayName from '../../mastodon/components/display_name';
 import Permalink from '../../mastodon/components/permalink';
 import ImmutablePureComponent from 'react-immutable-pure-component';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { makeGetAccount } from '../../mastodon/selectors';
+import ga from '../actions/ga';
+
+const gaCategory = 'FollowersYouFollow';
 
 const makeMapStateToProps = () => {
   const getAccount = makeGetAccount();
@@ -21,20 +23,27 @@ const makeMapStateToProps = () => {
 export default class FollowersYouFollowColumn extends ImmutablePureComponent {
 
   static propTypes = {
-    accountId: PropTypes.string.isRequired,
+    account: ImmutablePropTypes.map.isRequired,
   };
+
+  handleAccountClick = () => {
+    const { account } = this.props;
+
+    ga.event({
+      eventCategory: gaCategory,
+      eventAction: 'ClickAccountIcon',
+      eventLabel: account.get('id'),
+    });
+  }
 
   render() {
     const { account } = this.props;
 
     return (
-      <div className='account multi-column'>
-        <div className='account__wrapper'>
-          <Permalink key={account.get('id')} className='account__display-name' href={account.get('url')} to={`/accounts/${account.get('id')}`}>
-            <div className='account__avatar-wrapper'><Avatar account={account} size={36} /></div>
-            <DisplayName account={account} />
-          </Permalink>
-        </div>
+      <div className='multi-column'>
+        <Permalink key={account.get('id')} className='account__display-name' href={account.get('url')} to={`/accounts/${account.get('id')}`} onInterceptClick={this.handleAccountClick}>
+          <div className='account__avatar-wrapper'><Avatar account={account} size={40} /></div>
+        </Permalink>
       </div>
     );
   }
