46474b100df45a22db8501eff75b980576d902f3
abcang <abcang1015@gmail.com>
2018-09-21 10:54:50 +0900

    fix test

--- spec/controllers/pawoo/extensions/home_controller_spec.rb
+++ spec/controllers/pawoo/extensions/home_controller_spec.rb
@@ -7,7 +7,31 @@ describe HomeController, type: :controller do
     subject { get :index }
 
     context 'when user has not signed in' do
-      it { is_expected.to redirect_to about_path }
+      before do
+        allow(controller).to receive(:root_path?).and_return(is_root_path)
+      end
+
+      context 'when path is root_path' do
+        let(:is_root_path) { true }
+
+        it 'does not store current location' do
+          subject
+          expect(controller.stored_location_for(:user)).to eq nil
+        end
+
+        it { is_expected.to redirect_to about_path }
+      end
+
+      context 'when path is not root_path' do
+        let(:is_root_path) { false }
+
+        it 'does not store current location' do
+          subject
+          expect(controller.stored_location_for(:user)).to be_present
+        end
+
+        it { is_expected.to redirect_to about_path }
+      end
     end
   end
 end
