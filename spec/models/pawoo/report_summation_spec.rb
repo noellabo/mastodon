09b9fa82d92e40e73a6b788712d6370c3f5a1d82
abcang <abcang1015@gmail.com>
2018-09-27 20:36:19 +0900

    Fix report summation

--- spec/models/pawoo/report_summation_spec.rb
+++ spec/models/pawoo/report_summation_spec.rb
@@ -11,17 +11,20 @@ RSpec.describe Pawoo::ReportSummation, type: :model do
     let!(:prohibited_report) { Fabricate(:report, pawoo_report_type: :prohibited, created_at: log_created_at) }
     let!(:reproduction_report) { Fabricate(:report, pawoo_report_type: :reproduction, created_at: log_created_at) }
     let!(:nsfw_report) { Fabricate(:report, pawoo_report_type: :nsfw, created_at: log_created_at) }
+    let!(:nsfw_multi_report) { Fabricate(:report, pawoo_report_type: :nsfw, created_at: log_created_at, status_ids: [1, 2, 3]) }
     let!(:donotlike_report1) { Fabricate(:report, pawoo_report_type: :donotlike, created_at: log_created_at) }
     let!(:donotlike_report2) { Fabricate(:report, pawoo_report_type: :donotlike, created_at: log_created_at) }
-    let!(:old_report) { Fabricate(:report, pawoo_report_type: :other, created_at: 3.day.ago) }
+    let!(:old_report) { Fabricate(:report, pawoo_report_type: :other, created_at: 3.days.ago) }
 
-    it { expect(subject.date).to eq time.to_date }
-    it { expect(subject.total_count).to eq 6 }
-    it { expect(subject.other_count).to eq 1 }
-    it { expect(subject.prohibited_count).to eq 1 }
-    it { expect(subject.reproduction_count).to eq 1 }
-    it { expect(subject.spam_count).to eq 0 }
-    it { expect(subject.nsfw_count).to eq 1 }
-    it { expect(subject.donotlike_count).to eq 2 }
+    it do
+      expect(subject.date).to eq time.to_date
+      expect(subject.total_count).to eq 9
+      expect(subject.other_count).to eq 1
+      expect(subject.prohibited_count).to eq 1
+      expect(subject.reproduction_count).to eq 1
+      expect(subject.spam_count).to eq 0
+      expect(subject.nsfw_count).to eq 4
+      expect(subject.donotlike_count).to eq 2
+    end
   end
 end
