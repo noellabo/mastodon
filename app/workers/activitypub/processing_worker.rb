531bf48991b95e8a26e917c847413501837cb88a
abcang <abcang1015@gmail.com>
2018-12-29 17:57:02 +0900

    cancel busy job

--- app/workers/activitypub/processing_worker.rb
+++ app/workers/activitypub/processing_worker.rb
@@ -6,6 +6,16 @@ class ActivityPub::ProcessingWorker
   sidekiq_options backtrace: true
 
   def perform(account_id, body)
+    return if cancelled?
+
     ActivityPub::ProcessCollectionService.new.call(body, Account.find(account_id), override_timestamps: true)
+  end
+
+  def cancelled?
+    Sidekiq.redis { |c| c.exists("cancelled-#{jid}") }
+  end
+
+  def self.cancel!(jid)
+    Sidekiq.redis { |c| c.setex("cancelled-#{jid}", 86400, 1) }
   end
 end
