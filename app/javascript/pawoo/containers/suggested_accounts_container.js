1068bb8c1d7bb0440a8c2b30c951ca2140e5ce86
abcang <abcang1015@gmail.com>
2019-02-19 17:42:42 +0900

    fix lint

--- app/javascript/pawoo/containers/suggested_accounts_container.js
+++ app/javascript/pawoo/containers/suggested_accounts_container.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
